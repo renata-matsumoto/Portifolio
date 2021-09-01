e8b7b7e5e18588114964a10fd3b1a0d20c082179
RenataS Matsumoto <renatas.matsumoto@gmail.com>
2021-08-31 23:39:11 -0300

    Alteração do debug

--- lib/pages/primeira_page.dart
+++ lib/pages/primeira_page.dart
@@ -14,26 +14,25 @@ class PrimeiraPage extends StatefulWidget {
 class _PrimeiraPageState extends State<PrimeiraPage> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          // title: Text('Renata'),
-          backgroundColor: Colors.black87,
-        ),
-        body: Stack(children: [
-          Container(
-              // height: MediaQuery.of(context).size.height,
-              // width: MediaQuery.of(context).size.width,
-              decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage('assets/images/renata1.png'),
-              fit: BoxFit.cover,
-            ),
-          )),
-          Container(
-              alignment: Alignment.bottomLeft,
-              child: Text(
-                ''' 
+    return Scaffold(
+      appBar: AppBar(
+        // title: Text('Renata'),
+        backgroundColor: Colors.black87,
+      ),
+      body: Stack(children: [
+        Container(
+            // height: MediaQuery.of(context).size.height,
+            // width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/renata1.png'),
+            fit: BoxFit.cover,
+          ),
+        )),
+        Container(
+            alignment: Alignment.bottomLeft,
+            child: Text(
+              ''' 
 
        Olá, 
 
@@ -45,135 +44,132 @@ class _PrimeiraPageState extends State<PrimeiraPage> {
       
       
       ''',
-                style: GoogleFonts.yanoneKaffeesatz(
-                    textStyle: TextStyle(
-                        color: Color(0Xff07F2C7),
-                        shadows: [
-                          BoxShadow(
-                              color: Color(0xFFF22259),
-                              offset: Offset(1, 1),
-                              blurRadius: 25.0,
-                              spreadRadius: 15.0)
-                        ],
-                        fontSize: 32,
-                        fontWeight: FontWeight.bold,
-                        letterSpacing: 1)),
-              )),
-          // Container(
-          //   alignment: Alignment.topRight,
-          //   padding: EdgeInsets.all(10),
-          //   child: FloatingActionButton(
-          //     backgroundColor: Color(0xFF0FBF9F),
-          //     onPressed: () async =>
-          //         await launch("https://wa.me/${5511972345183}?text=Hello"),
-          //     child: FaIcon(
-          //       FontAwesomeIcons.comment,
-          //       color: Color(0xffF2F2F2),
-          //     ),
-          //   ),
-          // ),
-        ]),
-        drawer: Drawer(
-          child: Container(
-              padding: EdgeInsets.fromLTRB(0, 100, 0, 0),
-              color: Colors.black,
-              child: ListView(children: <Widget>[
-                ListTile(
+              style: GoogleFonts.yanoneKaffeesatz(
+                  textStyle: TextStyle(
+                      color: Color(0Xff07F2C7),
+                      shadows: [
+                        BoxShadow(
+                            color: Color(0xFFF22259),
+                            offset: Offset(1, 1),
+                            blurRadius: 25.0,
+                            spreadRadius: 15.0)
+                      ],
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 1)),
+            )),
+        // Container(
+        //   alignment: Alignment.topRight,
+        //   padding: EdgeInsets.all(10),
+        //   child: FloatingActionButton(
+        //     backgroundColor: Color(0xFF0FBF9F),
+        //     onPressed: () async =>
+        //         await launch("https://wa.me/${5511972345183}?text=Hello"),
+        //     child: FaIcon(
+        //       FontAwesomeIcons.comment,
+        //       color: Color(0xffF2F2F2),
+        //     ),
+        //   ),
+        // ),
+      ]),
+      drawer: Drawer(
+        child: Container(
+            padding: EdgeInsets.fromLTRB(0, 100, 0, 0),
+            color: Colors.black,
+            child: ListView(children: <Widget>[
+              ListTile(
+                leading: FaIcon(
+                  FontAwesomeIcons.user,
+                  size: 28,
+                  color: Colors.white,
+                ),
+                title: Text('Sobre',
+                    style: TextStyle(color: Colors.white, fontSize: 20)),
+                subtitle: Text('Renata Matsumoto...',
+                    style: TextStyle(color: Colors.white, fontSize: 16)),
+                trailing: IconButton(
+                  icon: FaIcon(FontAwesomeIcons.arrowRight,
+                      size: 25, color: Colors.white),
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => Sobre()));
+                  },
+                ),
+              ),
+              ListTile(
                   leading: FaIcon(
-                    FontAwesomeIcons.user,
+                    FontAwesomeIcons.lightbulb,
                     size: 28,
                     color: Colors.white,
                   ),
-                  title: Text('Sobre',
+                  title: Text('Meus Projetos',
                       style: TextStyle(color: Colors.white, fontSize: 20)),
-                  subtitle: Text('Renata Matsumoto...',
+                  subtitle: Text(' Projetos...',
                       style: TextStyle(color: Colors.white, fontSize: 16)),
                   trailing: IconButton(
                     icon: FaIcon(FontAwesomeIcons.arrowRight,
                         size: 25, color: Colors.white),
                     onPressed: () {
                       Navigator.push(context,
-                          MaterialPageRoute(builder: (context) => Sobre()));
+                          MaterialPageRoute(builder: (context) => Projetos()));
                     },
+                  )),
+              ListTile(
+                  leading: FaIcon(
+                    FontAwesomeIcons.mobileAlt,
+                    size: 28,
+                    color: Colors.white,
                   ),
-                ),
-                ListTile(
-                    leading: FaIcon(
-                      FontAwesomeIcons.lightbulb,
-                      size: 28,
-                      color: Colors.white,
-                    ),
-                    title: Text('Meus Projetos',
-                        style: TextStyle(color: Colors.white, fontSize: 20)),
-                    subtitle: Text(' Projetos...',
-                        style: TextStyle(color: Colors.white, fontSize: 16)),
-                    trailing: IconButton(
-                      icon: FaIcon(FontAwesomeIcons.arrowRight,
-                          size: 25, color: Colors.white),
-                      onPressed: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => Projetos()));
-                      },
-                    )),
-                ListTile(
-                    leading: FaIcon(
-                      FontAwesomeIcons.mobileAlt,
-                      size: 28,
-                      color: Colors.white,
-                    ),
-                    title: Text('Contato',
-                        style: TextStyle(color: Colors.white, fontSize: 20)),
-                    trailing: IconButton(
-                      icon: FaIcon(FontAwesomeIcons.arrowRight,
-                          size: 25, color: Colors.white),
-                      onPressed: () {
-                        Navigator.push(context,
-                            MaterialPageRoute(builder: (context) => Contact()));
-                      },
-                    )),
-                Container(
-                  color: Colors.black12,
-                  height: 500,
-                  child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children: [
-                        IconButton(
-                            icon: FaIcon(
-                              FontAwesomeIcons.linkedinIn,
-                              size: 30,
-                              color: Color(0xff07F2C7),
-                            ),
-                            onPressed: () async => await launch(
-                                "https://www.linkedin.com/in/renatas-matsumoto/")),
-                        SizedBox(
-                          width: 10,
-                        ),
-                        IconButton(
-                            icon: FaIcon(
-                              FontAwesomeIcons.githubAlt,
-                              size: 28,
-                              color: Color(0xff07F2C7),
-                            ),
-                            onPressed: () async => await launch(
-                                "https://github.com/renata-matsumoto")),
-                        SizedBox(
-                          width: 10,
-                        ),
-                        IconButton(
-                            icon: FaIcon(
-                              FontAwesomeIcons.instagram,
-                              size: 28,
-                              color: Color(0xff07F2C7),
-                            ),
-                            onPressed: () async =>
-                                await launch("https://instagram.com")),
-                      ]),
-                )
-              ])),
-        ),
+                  title: Text('Contato',
+                      style: TextStyle(color: Colors.white, fontSize: 20)),
+                  trailing: IconButton(
+                    icon: FaIcon(FontAwesomeIcons.arrowRight,
+                        size: 25, color: Colors.white),
+                    onPressed: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => Contact()));
+                    },
+                  )),
+              Container(
+                color: Colors.black12,
+                height: 500,
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      IconButton(
+                          icon: FaIcon(
+                            FontAwesomeIcons.linkedinIn,
+                            size: 30,
+                            color: Color(0xff07F2C7),
+                          ),
+                          onPressed: () async => await launch(
+                              "https://www.linkedin.com/in/renatas-matsumoto/")),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      IconButton(
+                          icon: FaIcon(
+                            FontAwesomeIcons.githubAlt,
+                            size: 28,
+                            color: Color(0xff07F2C7),
+                          ),
+                          onPressed: () async => await launch(
+                              "https://github.com/renata-matsumoto")),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      IconButton(
+                          icon: FaIcon(
+                            FontAwesomeIcons.instagram,
+                            size: 28,
+                            color: Color(0xff07F2C7),
+                          ),
+                          onPressed: () async =>
+                              await launch("https://instagram.com")),
+                    ]),
+              )
+            ])),
       ),
     );
   }
